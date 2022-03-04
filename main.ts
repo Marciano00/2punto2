70b21c3ab58e7704c1ec020079e88eedc72e89ee
Marciano00 <99951432+Marciano00@users.noreply.github.com>
2022-03-04 08:44:19 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showString("" + (randint(0, 100)))
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showString("" + (Math.randomBoolean()))
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(500)
+})
